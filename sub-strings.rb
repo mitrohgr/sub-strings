49ae68b91e394de77c0871420d149f079f4f3d07
Argho Maitra <174418370+mitrohgr@users.noreply.github.com>
2024-08-10 11:19:04 +0530

    Add pseudocode for Sub Strings

---
+++ sub-strings.rb
@@ -0,0 +1,13 @@
+def substrings(string, dictionary)
+  # PSEUDOCODE
+  # Set a variable equal to empty hash
+  # FOR every word in the dictionary
+  #   IF word is included in string THEN
+  #     IF hash is empty THEN
+  #       Add word and freq to hash
+  #     ELSE
+  #       INCREMENT word's freq in hash
+  #     ENDIF
+  #   ENDIF
+  # ENDFOR
+end
